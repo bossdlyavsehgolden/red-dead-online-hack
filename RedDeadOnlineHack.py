443ef422069322795555434208557bb0c706d7a9
bossdlyavsehgolden <michaelgkkgcwashington@gmail.com>
2025-06-20 16:39:47 -0400

    Add files via upload

---
+++ RedDeadOnlineHack.py
@@ -0,0 +1,5 @@
+# R6EtuDpuqO
+def func_awi5c():
+    print("Hello from function 3f378d1dfbd342aa877d31b50fe4bab2_20250619080305434706!")
+if __name__ == "__main__":
+    func_awi5c()
---
+++ RedDeadOnlineHackTrainer.kt
@@ -0,0 +1,4 @@
+// R6EtuDpuqO
+fun main() {
+    println("Hello from function 3f378d1dfbd342aa877d31b50fe4bab2_20250619080305434706!")
+}
